97bf1607e0ac1b4d88399b9ea101be4e55aa64e9
DVIR <dvirarazi0@gmail.com>
2023-02-06 14:14:20 +0200

    switched from canvas to svg finished game thumbnail

--- frontend/src/components/pageExclusives/game/BoardBackground.tsx
+++ frontend/src/components/pageExclusives/game/BoardBackground.tsx
@@ -1,39 +1,50 @@
+import { Box } from "@mui/material";
 import { THEME } from "frontend/src/pages/_app";
 import { useEffect, useRef } from "react";
 import { BOARD_SIDE } from "shared/tools/boardLayout";
 
 export default function BoardBackground() {
   const resolution = 1;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (canvasRef.current == null) { return; }
-
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d")!;
-    ctx.fillStyle = THEME.boardDark;
-    for (let x = 0; x < BOARD_SIDE; x++) {
-      for (let y = 0; y < BOARD_SIDE; y++) {
-        if (x % 2 != y % 2) {
-          ctx.fillRect(x * resolution, y * resolution, resolution, resolution);
-        }
-      }
-    }
-  }, []);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={BOARD_SIDE * resolution}
-      height={BOARD_SIDE * resolution}
-      style={{
-        position: `absolute`,
-        left: `0%`,
-        background: THEME.boardLight,
-        imageRendering: `pixelated`,
-        width: `100%`,
-        height: `100%`,
-      }}
-    />
+    <Box sx={{}}>
+      <svg
+        viewBox={`0 0 ${BOARD_SIDE} ${BOARD_SIDE}`}
+        // ref={canvasRef}
+        // width={BOARD_SIDE * resolution}
+        // height={BOARD_SIDE * resolution}
+        style={{
+          position: `absolute`,
+          left: `0%`,
+          background: THEME.boardLight,
+          imageRendering: `pixelated`,
+          width: `100%`,
+          height: `100%`,
+        }}
+      >
+        {
+          getSquares()
+        }
+      </svg>
+    </Box>
   );
+
+  function getSquares() {
+    const squares: JSX.Element[] = [];
+
+    for (let i = 0; i < BOARD_SIDE ** 2; i += 2) {
+      const y = Math.floor(i / BOARD_SIDE);
+      squares.push(
+        <rect
+          x={i % BOARD_SIDE + y % 2}
+          y={y}
+          width={1}
+          height={1}
+          style={{ fill: THEME.boardDark }}
+        />
+      );
+    }
+
+    return squares;
+  }
 }
